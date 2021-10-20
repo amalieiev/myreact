975436189920be65cc5c19264f828a1c9399db5a
Artem Malieiev <artem.malieiev@namecheap.com>
2021-10-20 23:17:47 +0300

    updated

--- components/App.js
+++ components/App.js
@@ -1,6 +1,17 @@
+import { useState } from "../react.js";
+
 export default function ({ count }) {
+    const [value, setValue] = useState(10);
+
     return `
         <h2>App Component</h2>
         <p>Count: ${count}</p>
+        <p>Props: ${value}</p>
+    `;
+}
+
+function User() {
+    return `
+        <p>User</p>
     `;
 }
--- react.js
+++ react.js
@@ -1,12 +1,34 @@
 let globalId = 0;
+let globalParent;
 const componentState = new Map();
 
 export function render(component, props, parent) {
+    const state = componentState.get(parent) || { cache: [] };
+
+    componentState.set(parent, { ...state, component, props });
+
+    globalParent = parent;
     const content = component(props);
     parent.innerHTML = content;
     globalId = 0;
 }
 
 export function useState(initialState) {
-    return [initialState, () => {}];
+    const id = globalId;
+    const parent = globalParent;
+
+    return (() => {
+        const { cache, component, props } = componentState.get(parent);
+
+        if (cache[id] == null) {
+            cache[id] = { value: initialState };
+        }
+
+        const setState = (state) => {
+            cache[id].value = state;
+            render(component, props, parent);
+        };
+
+        return [cache[id].value, setState];
+    })();
 }
