bc3bf1362d8e53f37b8f804ed54c4e4b18fb7fd6
artem.malieiev <artem.malieiev@exclaimer.com>
2021-10-28 15:55:32 +0300

    updated structure

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+require("dotenv").config();
+
+const urlPlaceholder = "{blobStore}";
+
+module.exports = async (env, options) => {
+    return {
+        devtool: "source-map",
+        entry: {
+            main: "./src/index.ts",
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: "ts-loader",
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: "./src/index.html",
+                chunks: ["main"],
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        to: "[name].[ext]",
+                        from: "manifest.xml",
+                        transform(content) {
+                            return content
+                                .toString()
+                                .replace(
+                                    new RegExp(urlPlaceholder, "g"),
+                                    process.env.URL
+                                );
+                        },
+                    },
+                    {
+                        from: "./assets",
+                        to: "assets",
+                        force: true,
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+        ],
+        devServer: {
+            open: true,
+            port: 3000,
+        },
+    };
+};
