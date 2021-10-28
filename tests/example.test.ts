7f39cf6e9caa0dd722253f82a14096ec7fe4e0f8
Artem Malieiev <artem.malieiev@namecheap.com>
2021-10-28 22:50:45 +0300

    added success tests

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -5,56 +5,26 @@ import {
     getByText,
     getByTestId,
     queryByTestId,
-    // Tip: all queries are also exposed on an object
-    // called "queries" which you could import here as well
+    queries,
     waitFor,
+    queryByText,
 } from "@testing-library/dom";
 // adds special assertions like toHaveTextContent
 import "@testing-library/jest-dom";
-import { App } from "../src";
-import { render } from "../src/core";
 
-function getExampleDOM() {
-    // This is just a raw example of setting up some DOM
-    // that we can interact with. Swap this with your UI
-    // framework of choice 😉
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <label for="username">Username</label>
-    <input id="username" />
-    <button>Print Username</button>
-  `;
-    const button = div.querySelector("button");
-    const input = div.querySelector("input");
-    button.addEventListener("click", () => {
-        // let's pretend this is making a server request, so it's async
-        // (you'd want to mock this imaginary request in your unit tests)...
-        setTimeout(() => {
-            const printedUsernameContainer = document.createElement("div");
-            printedUsernameContainer.innerHTML = `
-        <div data-testid="printed-username">${input.value}</div>
-      `;
-            div.appendChild(printedUsernameContainer);
-        }, Math.floor(Math.random() * 200));
-    });
-    return div;
-}
+test("examples of some things", async () => {
+    const container = document.createElement("div");
+    container.innerHTML = "<span>Print Username</span>";
 
-test("should render input element", async () => {
-    const container = getExampleDOM();
+    setTimeout(() => {
+        container.innerHTML = "<button>Success</button>";
+    }, 400);
 
-    render(App, {}, container);
+    expect(container).toMatchSnapshot();
 
-    // Get form elements by their label text.
-    // An error will be thrown if one cannot be found (accessibility FTW!)
-    // const input: HTMLInputElement = getByLabelText(container, "username");
-    // input.value = "Some Text";
+    expect(getByText(container, /Print Username/)).toBeTruthy();
 
-    // await waitFor(() => expect(queryByTestId(container, "output")).toBeTruthy());
+    await waitFor(() => expect(queryByText(container, /Success/)).toBeTruthy());
 
-    // getByTestId and queryByTestId are an escape hatch to get elements
-    // by a test id (could also attempt to get this element by its text)
-    // expect(getByTestId(container, "printed-username")).toHaveTextContent(famousProgrammerInHistory);
-    // jest snapshots work great with regular DOM nodes!
     expect(container).toMatchSnapshot();
 });
