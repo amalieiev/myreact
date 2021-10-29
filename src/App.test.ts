41d927e1e287596d50b3f23fc40aff7c5507f084
artem.malieiev <artem.malieiev@exclaimer.com>
2021-10-29 11:01:26 +0300

    updated tests

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,44 +1,32 @@
-// src/__tests__/example.js
-// query utilities:
-import {
-    getByLabelText,
-    getByText,
-    getByTestId,
-    queryByTestId,
-    fireEvent,
-    queries,
-    waitFor,
-    queryByText,
-} from "@testing-library/dom";
-// adds special assertions like toHaveTextContent
+import { fireEvent, queries } from "@testing-library/dom";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
 
 import { App } from "./App";
 import { render } from "./core";
 
-test("testing App component", async () => {
-    const container = document.createElement("div");
+function typeToInput(input, text) {
+    text.split("").reduce((memo, value, index) => {
+        memo += value;
+        fireEvent.keyUp(input, { target: { value: memo } });
+        console.log(memo);
+        return memo;
+    }, "");
 
-    render(App, {}, container);
+    fireEvent.change(input, { target: { value: text } });
+}
 
-    expect(getByText(container, /Type Text/)).toBeTruthy();
+describe("App component", () => {
+    it("should render App", async () => {
+        const container = document.createElement("div");
 
-    const input = getByLabelText(container, /Type Text/);
+        render(App, {}, container);
 
-    // userEvent.type(input, "Hello World");
+        expect(queries.getByText(container, /Type Text/)).toBeTruthy();
 
-    fireEvent.keyUp(input, { target: { value: "H" } });
-    fireEvent.keyUp(input, { target: { value: "He" } });
-    fireEvent.keyUp(input, { target: { value: "Hel" } });
-    fireEvent.keyUp(input, { target: { value: "Hell" } });
-    fireEvent.keyUp(input, { target: { value: "Hello" } });
+        const input = queries.getByLabelText(container, /Type Text/);
 
-    // setTimeout(() => {
-    //     container.querySelector("p").innerHTML = "Hello World";
-    // }, 100);
+        typeToInput(input, "Hello typeToInput utility");
 
-    expect(container).toMatchSnapshot();
-
-    await waitFor(() => expect(queryByText(container, /Hello/)).toBeTruthy());
+        expect(await queries.findByText(container, /Hello/)).toBeTruthy();
+    });
 });
